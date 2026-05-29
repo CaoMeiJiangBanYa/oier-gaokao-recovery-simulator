a48586d7a71269bfb6618cbdef1b5ad6aceeeb1c
LEOOOOOOOOOISME <15021960232@163.com>
2026-05-29 22:13:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/oier-gaokao-recovery-simulator/', 
+  // 必须和你的仓库名完全一致
+  base: '/oier-gaokao-recovery-simulator/',
   plugins: [react()],
+  build: {
+    emptyOutDir: true
+  }
 })

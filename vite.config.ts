acc00ae145664346dfbee2fc39bb38811f4f0b86
LEOOOOOOOOOISME <15021960232@163.com>
2026-05-29 22:31:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: process.env.GITHUB_ACTIONS === 'true' ? '/oier-gaokao-recovery-simulator/' : './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {

6e160fd0e4e1201d8ca5ff6e3f9b61fce522b4d7
LEOOOOOOOOOISME <15021960232@163.com>
2026-05-29 22:17:37 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Simulator from './pages/Simulator'
+import NotFound from './pages/NotFound'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+root.render(
+  <React.StrictMode>
+    {/* 👇 关键：basename 和仓库名完全一致 */}
+    <BrowserRouter basename="/oier-gaokao-recovery-simulator">
+      <Routes>
+        {/* 首页 */}
+        <Route path="/" element={<Home />} />
+
+        {/* 子页面 */}
+        <Route path="/about" element={<About />} />
+        <Route path="/simulator" element={<Simulator />} />
+
+        {/* 404 兜底 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
